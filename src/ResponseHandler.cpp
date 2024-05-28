4e61e45313524d5ba0c42295506d1ff297d12370
joeroeg <6369633@gmail.com>
2024-05-28 11:23:41 -0400

    fixed gcc issues on linux

--- src/ResponseHandler.cpp
+++ src/ResponseHandler.cpp
@@ -39,23 +39,63 @@ void sendReplyMsg(int fd, ...) {
 }
 
 std::string current_timestamp() {
-	std::time_t now = std::time(0);
+    std::time_t now = std::time(0);
     std::tm now_tm;
     localtime_r(&now, &now_tm);
+    char buffer[20];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &now_tm);
+
     std::ostringstream oss;
-    oss << "[" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "]";
+    oss << "[" << buffer << "]";
     return oss.str();
 }
+
+// void sendServerMsg(const char* format, ...) {
+//     va_list args;
+//     va_start(args, format);
+
+//     char messageBuffer[1024];
+// 	vsnprintf(messageBuffer, sizeof(messageBuffer), format, args);
+//     va_end(args);
+
+//     std::string timestamp = current_timestamp();
+//     std::string fullMessage = timestamp + " " + messageBuffer + "\n";
+//     std::cout << fullMessage;
+// }
 
 void sendServerMsg(const char* format, ...) {
     va_list args;
     va_start(args, format);
 
-    char messageBuffer[1024];
-	vsnprintf(messageBuffer, sizeof(messageBuffer), format, args);
-    va_end(args);
+    std::ostringstream oss;
+    oss << current_timestamp() << " ";
 
-    std::string timestamp = current_timestamp();
-    std::string fullMessage = timestamp + " " + messageBuffer + "\n";
-    std::cout << fullMessage;
+    while (*format != '\0') {
+        if (*format == '%' && *(format + 1) != '\0') {
+            format++;
+            switch (*format) {
+                case 'd': {
+                    int i = va_arg(args, int);
+                    oss << i;
+                    break;
+                }
+                case 's': {
+                    const char* s = va_arg(args, const char*);
+                    oss << s;
+                    break;
+                }
+                default: {
+                    oss << '%' << *format;
+                    break;
+                }
+            }
+        } else {
+            oss << *format;
+        }
+        format++;
+    }
+
+    va_end(args);
+    oss << "\n";
+    std::cout << oss.str();
 }
