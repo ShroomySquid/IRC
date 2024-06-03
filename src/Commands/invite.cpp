8e0d64fe73a73b95b91faeeac972f3a840b24c86
Bruno-Pierre Bouchard <bbouchar@c3r8p2.42quebec.com>
2024-06-03 17:07:32 -0400

    INVITE command now sends invite message

--- src/Commands/invite.cpp
+++ src/Commands/invite.cpp
@@ -17,39 +17,41 @@ void Cmd_invite::execute(Server &server, Client& sender, std::vector<std::string
 
     std::string user = arguments[1];
     std::string channelname = arguments[2];
+    if (channelname[0] == '#')
+    {
+        channelname = channelname.substr(1, channelname.length());
+    }
     Channel* channel = server.getChannel(channelname);
 
     if (channel == NULL)
     {
+        
         //sendErrorMsg(sender.get_fd(), "403", "ERR_NOSUCHCHANNEL", ": No such channel\r\n");
-        std::string message = "403 ERR_NOSUCHCHANNEL : No such channel\r\n";
-        send(sender.get_fd(), message.c_str(), message.length(), 0);
+        sendReplyMsg(sender.get_fd(), ERR_NOSUCHCHANNEL, sender.get_nickname().c_str(), channelname.c_str(), ERR_NOSUCHCHANNEL_MSG, NULL);
         return;
     }
 
     if (channel->getMember_by_name(sender.get_username()) == NULL)
     {
-        std::string message = "442 ERR_NOTONCHANNEL : User not on channel\r\n";
-        send(sender.get_fd(), message.c_str(), message.length(), 0);
+        sendReplyMsg(sender.get_fd(), ERR_NOTONCHANNEL, sender.get_nickname().c_str(), channel->get_name().c_str(), ERR_NOTONCHANNEL_MSG, NULL);
         return;
     }
     if (channel->getMember_by_name(user))
     {
-        std::string message = "443 ERR_USERONCHANNEL : User already on channel\r\n";
-        send(sender.get_fd(), message.c_str(), message.length(), 0);
+        sendReplyMsg(sender.get_fd(), ERR_USERONCHANNEL, ERR_USERONCHANNEL_MSG, NULL);
         return;
     }
 
-    std::string message1 = sender.get_username() + " wants to invite you in channel " + channelname + "\r\n";
     Client *invited = server.find_client(user);
     if(invited == NULL)
     {
-        std::cout << "bug, impossible to find client" << std::endl;
+        std::cout << "impossible to find client" << std::endl;
         return;
     }
-    send(invited->get_fd(), message1.c_str(), message1.length(), 0);
-    std::string message2 = "341 RPL_INVITING : Invite sent to " + user + " to join channel " + channelname + "\r\n";
-    send(sender.get_fd(), message2.c_str(), message2.length(), 0);
+
+    std::string message1 = (":"+sender.get_nickname() + " PRIVMSG " + sender.get_client() + " wants to invite you in channel: " + channelname + "\r\n");
+    send(invited->get_fd(), message1.c_str() , message1.length(),0);
+    sendReplyMsg(sender.get_fd(), RPL_INVITING, sender.get_client().c_str(), invited->get_client().c_str(), ("#"+channelname).c_str(), NULL);
     channel->addInvited(invited);
 
 }
