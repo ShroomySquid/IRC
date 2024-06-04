aca640c1ff0ab2fe231a0a7feaddaf83d57e38c3
Bruno <BrunoPierreBouchard@hotmail.ca>
2024-06-04 18:25:17 -0400

    Quit weechat response

--- src/Commands/quit.cpp
+++ src/Commands/quit.cpp
@@ -7,6 +7,24 @@ Cmd_quit::~Cmd_quit(){}
 
 void Cmd_quit::execute(Server &server, Client& sender, std::vector<std::string> arguments)
 {
+
+	// --------- broadcast a message to all users
+	std::string quitmessage;
+	if (arguments.size() > 1)
+		quitmessage = ":" + sender.get_client() + " QUIT " + "Quit: " + arguments[1] + "\r\n";
+	else
+		quitmessage = ":" + sender.get_client() + " QUIT " + "Quit: " + "\r\n";
+
+	std::map<int, Client*> clients =  server.get_clients();
+	std::map<int, Client*>::iterator it2;
+	for (it2 = clients.begin(); it2 != clients.end(); it2++)
+	{
+		Client* c = (*it2).second;
+		send(c->get_fd(),quitmessage.c_str(), quitmessage.length(),0);
+	}
+	// --------------------------
+
+	//disconnect sender from all channels
     std::map <std::string, Channel*> channels = server.get_all_channels();
     std::map <std::string, Channel*>::iterator it;
 	for (it = channels.begin(); it != channels.end(); it++)
@@ -15,18 +33,4 @@ void Cmd_quit::execute(Server &server, Client& sender, std::vector<std::string> 
 		c->removeClient(&sender);
 	}
 	sender.disconnect();
-	// broadcast a message to all users
-	std::string quitmessage = std::string();
-	quitmessage += sender.get_client();
-	quitmessage += " Quit: ";
-	if (arguments.size() > 1)
-		quitmessage += arguments[1];
-	std::map<int, Client*> clients =  server.get_clients();
-	std::map<int, Client*>::iterator it2;
-	for (it2 = clients.begin(); it2 != clients.end(); it2++)
-	{
-		Client* c = (*it2).second;
-		sendReplyMsg(c->get_fd(), quitmessage.c_str(), NULL);
-	}
-
 }
